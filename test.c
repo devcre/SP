6dde7eb5c5676953b3f366c8929ee4b42cd85255
devcre <tigertank707@gmail.com>
2019-04-05 15:42:09 +0900

    branch

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	printf("This is test file for git"\n);
+	printf("This is test file for git\n");
+	printf("This is more third version of git file\n");
 	return 0;
 }
